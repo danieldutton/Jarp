af78fb8d357cdbd77884d289f785d1f4ea67dc6f
Daniel Dutton <danielbdutton@googlemail.com>
2014-01-14 22:11:20 +0000

    Added project Finite State Generator

---
+++ ProjectX.FiniteStateGenerator/Terminal.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectX.FiniteStateGenerator
+{
+    public class Terminal<T>
+    {
+        public T Input { get; private set; }
+
+        public T Output { get; private set; }
+
+        public Terminal(T input, T output)
+        {
+            Input = input;
+            Output = output;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}] Input:{1} Output{2}"
+                , GetType().Name, Input, Output);
+        }
+    }
+}
